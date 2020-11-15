3538add2bfd5d7948ea0cd341c8df5380c72c1c0
v1rt0 <contato.santosvictor1@gmail.com>
2020-11-15 16:57:30 -0300

    creepycreepy0.2.0
    adicionado movimento do personagem

--- creepycreepy.py
+++ creepycreepy.py
@@ -1,35 +1,90 @@
-import pygame
+##import pygame
+##from pygame.locals import *
+##
+##tamTela = (800, 600)
+##corTela = (255,255,255)
+##background = pygame.image.load('bckgrd.jpg')
+##
+##pygame.init()
+##
+##tela = pygame.display.set_mode((tamTela), 0, 32)
+##pygame.display.set_caption("Creepy Creepy")
+##
+##
+##playerImg = pygame.image.load('mainchback.png')
+##playerX = 370
+##playerY = 480
+##
+##
+##def player(x, y):
+##    tela.blit(playerImg, (x, y))
+##    playerX += 5
+##
+##running = True
+##while running:
+##
+##    tela.fill(corTela)
+##
+##    for event in pygame.event.get():
+##        if event.type == pygame.QUIT:
+##            running = False
+##            
+##    player(playerX, playerY)
+##    pygame.display.update()
+##    
+
+from random import randint
+import pygame                                       
 from pygame.locals import *
 
-tamTela = (800, 600)
-corTela = (255,255,255)
-background = pygame.image.load('bckgrd.jpg')
+# Inicialização de dados
+LarX = 800
+AltY = 600
+X = 350
+Y = 500
 
-pygame.init()
+pygame.init()                                        
+tela = pygame.display.set_mode((LarX, AltY), 0, 32)     
+pygame.display.set_caption('Trabalhando com Imagens')        
 
-tela = pygame.display.set_mode((tamTela), 0, 32)
-pygame.display.set_caption("Creepy Creepy")
+fundo = pygame.image.load('bckgrd.jpg')
+personagem = pygame.image.load('mainchback.png')
 
-jogadorImgCostas = pygame.image.load('mainchback.png')
-imgCostasXY = (400, 480)
 
-def jogador():
-    tela.blit(jogadorImgCostas, (imgCostasXY))
 
-encerrar = False
-
-while not encerrar:
-    tela.fill(corTela)
-    tela.blit(background, (0, 0))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            encerrar = True
-                
+fim = False
+while not fim:
     
-    jogador()
+  tela.blit(fundo, (0, 0))
+  tela.blit(personagem, (X, Y))
+  pygame.display.update()                            
+  Teclas = pygame.key.get_pressed()
+  
+  
+  if Teclas[K_UP]:
+    Y = Y - 0.25
+    personagem = pygame.image.load('mainchback.png')
+    tela.blit(personagem, (X, Y))
     
-    pygame.display.update()
-   
-        
-          
-pygame.display.quit()
+  if Teclas[K_DOWN]:
+    Y = Y + 0.25
+    personagem = pygame.image.load('mainchfront.png')
+    tela.blit(personagem, (X, Y))
+  if Teclas[K_LEFT]:
+      
+    X = X - 0.25
+    personagem = pygame.image.load('mainchleft.png')
+    tela.blit(personagem, (X, Y))
+    
+  if Teclas[K_RIGHT]:
+    X = X + 0.25
+    personagem = pygame.image.load('mainchright.png')
+    tela.blit(personagem, (X, Y))
+
+
+  for event in pygame.event.get():                   
+    if event.type == QUIT:                           
+      fim = True                                    
+
+pygame.display.quit()  
+print("Fim do programa")
